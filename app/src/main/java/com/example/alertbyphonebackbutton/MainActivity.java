8abf66d89ff0b50efd24622f8ba21d2793fc8344
Swechha211 <bistaswechha@gmail.com>
2023-10-29 08:51:57 +0545

    commit for alert dialog by back button of phone

---
+++ app/src/main/java/com/example/alertbyphonebackbutton/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.alertbyphonebackbutton;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Welcome to alert dialog box");
+        builder.setTitle("We are happy to display this dialog");
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+}
