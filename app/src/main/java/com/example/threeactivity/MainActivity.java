d9e34b0ea986027614f8bb528ba625cbfcd95208
Swechha211 <bistaswechha@gmail.com>
2023-10-30 15:03:01 +0545

    commit for three activity

---
+++ app/src/main/java/com/example/threeactivity/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.threeactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+private Button btn;
+private TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn= findViewById(R.id.btn);
+        txt1 = findViewById(R.id.txt1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        txt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+}
