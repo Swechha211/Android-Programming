3443d3c241e59a8c8db5853290c17719b0428b70
Swechha211 <bistaswechha@gmail.com>
2023-10-30 13:53:01 +0545

    commit for popup menu

---
+++ app/src/main/java/com/example/popupmenu/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.popupmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu menu = new PopupMenu(MainActivity.this, btn);
+                menu.getMenuInflater().inflate(R.menu.pop_menu, menu.getMenu());
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        Toast.makeText(MainActivity.this, "Clicked " + menuItem.getTitle() ,Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                menu.show();
+
+            }
+        });
+    }
+}
